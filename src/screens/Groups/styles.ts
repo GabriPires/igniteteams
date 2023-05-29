aaef68a16eae88033b914fea528b3641646f9b52
Gabriel Pires <gabpiresdev@gmail.com>
2023-05-29 19:26:05 -0300

    extending default theme type

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,12 +2,12 @@ import styled from 'styled-components/native'
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #fff;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   align-items: center;
   justify-content: center;
 `
 
 export const Title = styled.Text`
   font-size: 24px;
-  color: ${(props) => props.theme.COLORS.GREEN_500};
+  color: ${({ theme }) => theme.COLORS.WHITE};
 `
