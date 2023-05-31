0616ee552e90ae9d290975dc9d0b5d9c3f0e343f
Gabriel Pires <gabpiresdev@gmail.com>
2023-05-31 19:49:40 -0300

    new player card component

---
+++ src/components/PlayerCard/style.ts
@@ -0,0 +1,32 @@
+import styled, { css } from 'styled-components/native'
+import { MaterialIcons } from '@expo/vector-icons'
+
+export const Container = styled.View`
+  width: 100%;
+  height: 56px;
+
+  flex-direction: row;
+  align-items: center;
+
+  background-color: ${({ theme }) => theme.COLORS.GRAY_500};
+  border-radius: 6px;
+  margin-bottom: 16px;
+`
+
+export const Name = styled.Text`
+  ${({ theme }) => css`
+    flex: 1;
+
+    color: ${theme.COLORS.WHITE};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-size: ${theme.FONT_SIZE.MD}px;
+  `}
+`
+
+export const Icon = styled(MaterialIcons).attrs(({ theme }) => ({
+  size: 24,
+  color: theme.COLORS.GRAY_200,
+}))`
+  margin-left: 16px;
+  margin-right: 4px;
+`
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,10 +6,11 @@ import { Input } from '@components/Input'
 import { Filter } from '@components/Filter'
 import { FlatList } from 'react-native'
 import { useState } from 'react'
+import { PlayerCard } from '@components/PlayerCard'
 
 export function Players() {
   const [team, setTeam] = useState<string>('Time A')
-  const [players, setPlayers] = useState<string[]>([])
+  const [players, setPlayers] = useState<string[]>(['Gabriel', 'Marcos'])
 
   return (
     <Container>
@@ -41,6 +42,14 @@ export function Players() {
 
         <PlayersCount>{players.length}</PlayersCount>
       </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+      />
     </Container>
   )
 }
