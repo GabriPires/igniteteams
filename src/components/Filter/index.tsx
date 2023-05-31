77520742f75d2236d5e8de6b96d54e3d7ec16867
Gabriel Pires <gabpiresdev@gmail.com>
2023-05-31 19:31:46 -0300

    new filter component

---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,15 @@
+import { TouchableOpacityProps } from 'react-native'
+import { Container, Title, FilterStyleProps } from './styles'
+
+type FilterProps = TouchableOpacityProps &
+  FilterStyleProps & {
+    title: string
+  }
+
+export function Filter({ title, isActive = false, ...props }: FilterProps) {
+  return (
+    <Container isActive={isActive} {...props}>
+      <Title>{title}</Title>
+    </Container>
+  )
+}
