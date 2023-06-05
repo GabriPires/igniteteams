b76fdb457e53d37021a5c37d3e839d689f6a9d2b
Gabriel Pires <gabpiresdev@gmail.com>
2023-06-05 19:40:07 -0300

    navigate to other pages

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,8 +3,15 @@ import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Button } from '@components/Button'
 import { Input } from '@components/Input'
+import { useNavigation } from '@react-navigation/native'
 
 export function NewGroup() {
+  const { navigate } = useNavigation()
+
+  function handleNew() {
+    navigate('players')
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -22,6 +29,7 @@ export function NewGroup() {
           style={{
             marginTop: 20,
           }}
+          onPress={handleNew}
         />
       </Content>
     </Container>
