f39a286ac9a874d883b1b519b8f86c9c16e8cd3d
Gabriel Pires <gabpiresdev@gmail.com>
2023-06-06 19:47:25 -0300

    navigate to players screen with selected group

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,13 @@
-import { Header } from '@components/Header'
-import { Container } from './styles'
-import { Highlight } from '@components/Highlight'
+import { Button } from '@components/Button'
 import { GroupCard } from '@components/GroupCard'
+import { Header } from '@components/Header'
+import { Highlight } from '@components/Highlight'
+import { ListEmpty } from '@components/ListEmpty'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
+import { getAllGroups } from '@storage/group/group-get-all'
 import { useCallback, useState } from 'react'
 import { FlatList } from 'react-native'
-import { ListEmpty } from '@components/ListEmpty'
-import { Button } from '@components/Button'
-import { useNavigation, useFocusEffect } from '@react-navigation/native'
-import { getAllGroups } from '@storage/group/group-get-all'
+import { Container } from './styles'
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
@@ -20,6 +20,10 @@ export function Groups() {
     } catch (error) {
       console.log(error)
     }
+  }
+
+  function handleOpenGroup(group: string) {
+    navigate('players', { group })
   }
 
   function handleNewGroup() {
@@ -41,7 +45,9 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        renderItem={({ item }) => (
+          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+        )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => (
           <ListEmpty message={'Que tal cadastrar a primeira turma?'} />
