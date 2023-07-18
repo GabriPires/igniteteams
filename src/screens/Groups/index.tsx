b35482cb5c881825cfc06c7e46bf447fb40a5533
Gabriel Pires <gabpiresdev@gmail.com>
2023-07-17 22:30:38 -0300

    loading state to groups screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -8,17 +8,22 @@ import { getAllGroups } from '@storage/group/group-get-all'
 import { useCallback, useState } from 'react'
 import { FlatList } from 'react-native'
 import { Container } from './styles'
+import { Loading } from '@components/Loading'
 
 export function Groups() {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
   const [groups, setGroups] = useState<string[]>([])
   const { navigate } = useNavigation()
 
   async function fetchGroups() {
     try {
+      setIsLoading(true)
       const data = await getAllGroups()
       setGroups(data)
     } catch (error) {
       console.log(error)
+    } finally {
+      setIsLoading(false)
     }
   }
 
@@ -41,19 +46,22 @@ export function Groups() {
       <Header />
       <Highlight title={'Turmas'} subtitle={'jogue com a sua turma'} />
 
-      <FlatList
-        data={groups}
-        keyExtractor={(item) => item}
-        showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => (
-          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
-        )}
-        contentContainerStyle={groups.length === 0 && { flex: 1 }}
-        ListEmptyComponent={() => (
-          <ListEmpty message={'Que tal cadastrar a primeira turma?'} />
-        )}
-      />
-
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <FlatList
+          data={groups}
+          keyExtractor={(item) => item}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+          )}
+          contentContainerStyle={groups.length === 0 && { flex: 1 }}
+          ListEmptyComponent={() => (
+            <ListEmpty message={'Que tal cadastrar a primeira turma?'} />
+          )}
+        />
+      )}
       <Button title={'Criar nova turma'} onPress={handleNewGroup} />
     </Container>
   )
